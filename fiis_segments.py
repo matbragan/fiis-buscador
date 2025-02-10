2211e84ab0647ed982e87565f3eaf41f1275fcca
matbragan <matheus.bs.oliveira@gmail.com>
2025-02-10 20:43:54 -0300

    feature: add segments dict with correction of fiis segments

---
+++ fiis_segments.py
@@ -0,0 +1,10 @@
+fiis_segments = {
+'AGRX11': 'Fiagro',
+'MALL11': 'Shoppings',
+'HGBS11': 'Shoppings',
+'KNHF11': 'Híbrido',
+'NEWL11': 'Logística',
+'HSLG11': 'Logística',
+'XPLG11': 'Logística',
+'HGLG11': 'Logística',
+}
