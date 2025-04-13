e5f15d866f063b7a6221617fb2c6e7c6e7007890
matbragan <matheus.bs.oliveira@gmail.com>
2025-04-12 22:32:20 -0300

    feature: add communications page

--- communications/constants.py
+++ communications/constants.py
@@ -1,1 +1,14 @@
-BASE_URL = 'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM'
+FNET_BASE_URL = 'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM'
+
+COMMUNICATIONS_FILE_NAME = 'communications'
+
+TICKERS = [
+    'NEWL11',
+    'BTLG11',
+    'TVRI11',
+    'RZTR11',
+    'RECR11',
+    'GGRC11',
+    'RZAT11',
+    'GARE11',
+]
