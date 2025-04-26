327690c44c0c3d151ab083c8a39396ca4049cbcd
matbragan <matheus.bs.oliveira@gmail.com>
2025-04-26 19:57:23 -0300

    chore: fundamentus scrape attempts

--- src/scrapes/fundamentus.py
+++ src/scrapes/fundamentus.py
@@ -15,28 +15,33 @@ log_format = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=log_format, level=logging.INFO)
 
 
-def get_fundamentus_data(max_retries: int = 20) -> pd.DataFrame:
+def get_fundamentus_data(
+        max_attempts: int = 20
+) -> pd.DataFrame:
     ''' 
     Obtém os dados de FIIs do Fundamentus.
+
+    Args:
+        max_attempts (int): Número de tentativas de obtenção dos dados.
 
     Returns:
         pd.DataFrame: Um DataFrame contendo os dados dos FIIs do Fundamentus.
     '''
-    retry = 0
+    attempt = 1
     while True:
         response = requests.get(FUNDAMENTUS_URL, headers=HEADERS)
         
         if response.status_code == 200:
             df = pd.read_html(response.content, decimal=',', thousands='.')[0]
-            logging.info(f'Tentativa {retry + 1}: Dados obtidos com sucesso!')
+            logging.info(f'Tentativa {attempt}: Dados obtidos com sucesso!')
             break
         elif response.status_code == 403:
-            if retry == max_retries:
-                logging.warning(f'Tentativas excedidas {max_retries}: Erro 403, a requisição foi bloqueada pelo site.')
+            if attempt == max_attempts:
+                logging.warning(f'Tentativas excedidas {attempt}: Erro 403, a requisição foi bloqueada pelo site.')
                 return
-            logging.error(f'Tentativa {retry + 1}: Erro 403, a requisição foi bloqueada pelo site. Tentando novamente...')
+            logging.error(f'Tentativa {attempt}: Erro 403, a requisição foi bloqueada pelo site. Tentando novamente...')
             time.sleep(2)
-            retry += 1
+            attempt += 1
         else:
             logging.error(f'Erro na requisição: {response.status_code}')
             return
